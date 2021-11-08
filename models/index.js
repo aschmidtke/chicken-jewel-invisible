51f1138b9f03e3ada42dbf22043d198cee2a6639
Drew Schmidtke <amschmidtke@gmail.com>
2021-11-08 15:25:33 -0500

    models structured

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const sequelize = require('../config/connection');
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+User.belongsToMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
