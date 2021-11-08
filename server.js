00a7dc3b3e198d8123c2ffd8a9a9e8f24f5c8e4b
Drew Schmidtke <amschmidtke@gmail.com>
2021-11-08 14:44:47 -0500

    initial handlebar working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-//const routes = require('./controllers');
+const routes = require('./controllers');
 //const path = require('path');
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 //const session = require('express-session');
 //const helpers = require('./utils/helpers');
 
@@ -12,7 +12,12 @@ const sequelize = require('./config/connection');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'));
 });
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
